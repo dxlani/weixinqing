822b0a05f58bf3cf3fcb7cfea456576e3fd52189
mymilkbottles <javaxz@163.com>
2017-06-18 20:49:22 +0800

    使用spring boot构建项目

---
+++ src/main/java/me/mymilkbottles/weixinqing/WeixinqingApplication.java
@@ -0,0 +1,12 @@
+package me.mymilkbottles.weixinqing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinqingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinqingApplication.class, args);
+	}
+}
---
+++ src/test/java/me/mymilkbottles/weixinqing/WeixinqingApplicationTests.java
@@ -0,0 +1,16 @@
+package me.mymilkbottles.weixinqing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeixinqingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
